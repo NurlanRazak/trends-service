94dbef5f090b49162ca78bcba6575a4faa4ad757
Nurlan <nurik.droid@gmail.com>
2019-10-06 23:32:05 +0600

    service discovery

--- src/main/java/kz/balalar/balalartrendsservice/BalalarTrendsServiceApplication.java
+++ src/main/java/kz/balalar/balalartrendsservice/BalalarTrendsServiceApplication.java
@@ -2,8 +2,10 @@ package kz.balalar.balalartrendsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BalalarTrendsServiceApplication {
 
 	public static void main(String[] args) {
